0479d152c73ad9780a83c0cd708e503ab419ae42
corysell <corysell@protonmail.com>
2023-11-10 14:03:36 -0600

    modified format

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -12,11 +12,10 @@ func TestAdder(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
-
 }
 
 func ExampleAdd() {
 	sum := Add(1, 5)
 	fmt.Println(sum)
-	// Output should be 6
+	// Output: 6
 }
